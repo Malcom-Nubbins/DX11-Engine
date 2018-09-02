cc97c4f38a6a340fccd949b56f27f432ecc337c5
Malcom-Nubbins <malcom.nubbins@gmail.com>
2018-09-02 23:26:04 +0100

    Deprecated the old OBJ Loader class

--- DirectX 11 Engine/Core/Loaders/OBJLoader.h
+++ DirectX 11 Engine/Core/Loaders/OBJLoader.h
@@ -9,20 +9,24 @@
 #include "../Handlers/MathsHandler.h"
 #include <sstream>
 
-namespace OBJLoader
+class [[deprecated("Use 'ModelLoader' for any model loading. That one supports multi-part objects")]] OBJLoader
 {
+public:
 	//The only method you'll need to call
-	ObjectMesh Load(std::wstring filename, ID3D11Device* _pd3dDevice, bool invertTexCoords = true);
+	[[deprecated("Use 'ModelLoader' for any model loading. That one supports multi-part objects")]] static ObjectMesh Load(std::wstring filename, ID3D11Device* _pd3dDevice, bool invertTexCoords = true);
 
+private:
 	//Helper methods for the above method
 	//Searhes to see if a similar vertex already exists in the buffer -- if true, we re-use that index
-	bool FindSimilarVertex(const SimpleVertex& vertex, std::map<SimpleVertex, UINT>& vertToIndexMap, UINT& index);
+	static bool FindSimilarVertex(const SimpleVertex& vertex, std::map<SimpleVertex, UINT>& vertToIndexMap, UINT& index);
 
 	//Re-creates a single index buffer from the 3 given in the OBJ file
-	void CreateIndices(const std::vector<XMFLOAT3>& inVertices, const std::vector<XMFLOAT2>& inTexCoords, const std::vector<XMFLOAT3>& inNormals, const std::vector<XMFLOAT3>& inTans, const std::vector<XMFLOAT3>& inBinorms,
+	static void CreateIndices(const std::vector<XMFLOAT3>& inVertices, const std::vector<XMFLOAT2>& inTexCoords, const std::vector<XMFLOAT3>& inNormals, const std::vector<XMFLOAT3>& inTans, const std::vector<XMFLOAT3>& inBinorms,
 		std::vector<UINT>& outIndices, std::vector<XMFLOAT3>& outVertices, std::vector<XMFLOAT2>& outTexCoords, std::vector<XMFLOAT3>& outNormals,
 		std::vector<XMFLOAT3>& outTangents, std::vector<XMFLOAT3>& outBinormals);
 
-	void CreateTangentsAndBinormals(TempVertex v1, TempVertex v2, TempVertex v3, XMFLOAT3& outTans, XMFLOAT3& outBinorms);
-	void CalculateNormal(XMFLOAT3 tangent, XMFLOAT3 binormal, XMFLOAT3 & normal);
+	static void CreateTangentsAndBinormals(TempVertex v1, TempVertex v2, TempVertex v3, XMFLOAT3& outTans, XMFLOAT3& outBinorms);
+	static void CalculateNormal(XMFLOAT3 tangent, XMFLOAT3 binormal, XMFLOAT3 & normal);
 };
+
+	
